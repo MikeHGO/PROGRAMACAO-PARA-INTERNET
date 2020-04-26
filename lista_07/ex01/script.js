3a3d48b640fc5a7cef0feb2e59dae55dcece88d5
MikeHGO <michaelhgo@gmail.com>
2020-04-26 18:29:20 -0300

    Lista 07 completa

--- lista_07/ex01/script.js
+++ lista_07/ex01/script.js
@@ -1,19 +1,15 @@
 const FORM = document.getElementById("contact_form");
 
-FORM.addEventListener('submit', event => {
-    event.preventDefault();
-    var fdata = {};
-    for (let index = 0; index < FORM.elements.length; index++) {
-        if(FORM.elements[index].name != "Button") {
-            fdata[FORM.elements[index].name] = FORM.elements[index].value;
-        }
-    }
-    
-    var fdata_JSON = JSON.stringify(fdata);    
-    
-    document.getElementById("container").appendChild(fdata);
+FORM.addEventListener("submit", (event) => {
+	event.preventDefault();
+	var fdata = {};
+	for (let index = 0; index < FORM.elements.length; index++) {
+		if (FORM.elements[index].name != "Button") {
+			fdata[FORM.elements[index].name] = FORM.elements[index].value;
+		}
+	}
 
-    console.log(fdata_JSON);
-    
-    // FORM.reset();
+	var fdata_JSON = JSON.stringify(fdata);
+	document.getElementById("01").innerHTML = fdata_JSON;
+	// FORM.reset();
 });
